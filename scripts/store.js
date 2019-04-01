2673a1ef74efa87d54ac20a9d1a54ce9ad999f95
Nick Lee <leexnick@gmail.com>
2019-04-01 09:38:06 -0700

    update store.js with item-related CRUD functions

--- scripts/store.js
+++ scripts/store.js
@@ -1,4 +1,4 @@
-/* global cuid */
+/* global cuid, Item */
 
 'use strict';
 
@@ -13,9 +13,45 @@ const store = (function() {
   let hideCheckedItems = false;
   let searchTerm = '';
 
+  function findById(id) {
+    return store.items.find( item => item.id === id );
+  }
+
+  function addItem(name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch (error) {
+      console.log(`Cannot add item: ${error.message}`);
+    }
+  }
+
+  function findAndToggleChecked(id) {
+    const foundItem = this.findById(id);
+    foundItem.checked = !foundItem.checked;
+  }
+
+  function findAndUpdateName(id, newName) {
+    try {
+      Item.validateName(newName);
+      findById(id).name = newName;
+    } catch (error) {
+      console.log(`Cannot update name: ${error.message}`);
+    }
+  }
+
+  function findAndDelete(id) {
+    this.items = this.items.filter( item => item.id !== id);
+  }
+
   return {
     items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   };
 }() );
